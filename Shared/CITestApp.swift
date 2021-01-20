3c75fdd6cb810eaae9bacd2980c3d96b8ef771ad
amr <amradel525@gmail.com>
2021-01-20 15:34:51 +0200

    Initial Commit

---
+++ Shared/CITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestApp.swift
+//  Shared
+//
+//  Created by Amr on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct CITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
